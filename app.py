6db90d600f53aeb436aed4481bb25ce8662edcbe
Nick3755 <165529171@qq.com>
2023-06-09 17:14:38 +0800

    Add minimal home page

---
+++ app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-　　
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+
+
+@app.route('/user/<name>')
+def user_page(name):
+    return f'User: {escape(name)}'
